a36151a33d7207b23884e5e4d5c192d205d9946d
Ugusu <ugur.mamedzade@gmail.com>
2024-01-31 20:53:48 +0100

    Create Competition

--- contracts/HoldersCollaborate.sol
+++ contracts/HoldersCollaborate.sol
@@ -3,9 +3,135 @@ pragma solidity ^0.8.19;
 
 import {Admin} from "./Admin.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Elements} from "./Elements.sol";
 
 contract HoldersCollaborate is Admin(msg.sender), Ownable(msg.sender) {
     constructor() {}
+    type Level is Elements.Level;
+    type Token is Elements.Token;
+    type Status is Elements.Status;
+    type Competitor is Elements.Competitor;
+    type Competition is Elements.Competition;
+
+    // Competition ID => Competition
+    uint256 public last_competitions_id;
+    mapping(uint256 => Competition) public competitions;
+
+    // EVENTS
+    event competitionCreated(
+        uint256 competitions_id,
+        address creator,
+        address owner,
+        Token[] tokens,
+        uint256 start,
+        uint256 end,
+        Level[] levels,
+        uint256 reward_percentage,
+        Competitor[] competitors,
+        uint256 total_usd,
+        Status status
+    );
+
+    // Creates new competitions.
+    function createCompetition(
+        address[] memory tokens,
+        uint256 start,
+        uint256 end,
+        uint256 reward_percentage,
+        uint256[] memory level_treshholds,
+        uint256[] memory level_minimums,
+        uint256[] memory level_maximums
+    ) external returns(bool){
+        require(block.timestamp<start, "Can't create competition in the past");
+        require(block.timestamp<end, "Can't create finished competition");
+        require(reward_percentage>=0 && reward_percentage<=10000, "Reward must be between 0.00 and 100.00 (0-10000)");
+        
+        Token[] memory newTokens = createTokens(tokens);
+        Level[] memory newLevels = createLevels(level_treshholds, level_minimums, level_maximums);
+
+        Competition memory newCompetition = Competition({
+            creator: msg.sender,
+            owner: msg.sender,
+            tokens: newTokens,
+            start: start,
+            end: end,
+            levels: newLevels,
+            reward_percentage: reward_percentage,
+            competitors: new Competitor[](0),
+            total_usd: 0,
+            status: Status.Upcoming
+        });
+
+        last_competitions_id++;
+        competitions[last_competitions_id] = newCompetition;
+
+        // Emit event with details
+        emit competitionCreated(
+            last_competitions_id,
+            msg.sender,
+            newCompetition.owner,
+            newCompetition.tokens,
+            newCompetition.start,
+            newCompetition.end,
+            newCompetition.levels,
+            newCompetition.reward_percentage,
+            newCompetition.competitors,
+            newCompetition.total_usd,
+            newCompetition.status
+        );
+
+        return true;
+    }
+
+    // Creates array of Tokens, with value 0 for each.
+    function createTokens(
+        address[] memory tokens
+    ) external returns(Token[]){
+        require(tokens.length >= 2, "At least 2 tokens required");
+        
+        Token[] memory newTokens = new Token[](tokens.length);
+        
+        for (uint256 i = 0; i < tokens.length; i++){
+            require(tokens[i]!=address(0), "Zero address can't be a token");
+            newTokens[i] = Token(tokens[i], 0);
+        }
+
+        return newTokens;
+    }
+
+    // Creates array of Levels, puts first level as active.
+    function createLevels(
+        uint256[] memory level_treshholds,
+        uint256[] memory level_minimums,
+        uint256[] memory level_maximums
+    ) external returns(Level[]){
+        require(level_treshholds.length >= 1, "At least one level is required");
+        require(
+            level_minimums.length == level_treshholds.length &&
+            level_maximums.length == level_treshholds.length,
+            "Number of level parameters must match"
+        );
+
+        Level[] memory newLevels = new Level[](level_treshholds.length);
+        
+        for (uint256 i = 0; i < level_treshholds.length; i++){
+            if (i > 0){
+                require(level_treshholds[i]>level_treshholds[i-1], "Next level must have higher treshhold than previous");
+            }
+            require(level_minimum[i]<=level_maximum[i], "Level minimum must be lower or equal to level maximum");
+
+            newLevels[i] = Level(
+                i,
+                level_treshholds[i],
+                level_minimums[i],
+                level_maximums[i],
+                false
+            );
+        }
+        newLevels[0].active=true;
+
+        return newLevels;
+    }
 
     function setAdmin(address admin, bool value) public override onlyOwner {
         admins[admin] = value;
